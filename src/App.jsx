6b30569e3e5d077e8c95f81313fe64bec9e6ede6
Sina Moradian <sinacsharp@gmail.com>
2023-04-01 03:45:30 +0330

    Add BrowserRouter & Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Route path="/" exact={true} component={HomePage} />
+        <Route path="/About-Us" component={AboutUs} />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
