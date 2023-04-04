1a6327852529114e0eb78841742805e1628fd5f9
Sina Moradian <sinacsharp@gmail.com>
2023-04-04 08:43:23 +0330

    Add a navigation section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
+import Navigation from "./components/Navigation";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navigation />
         <Route path="/" exact={true} component={HomePage} />
         <Route path="/About-Us" component={AboutUs} />
       </div>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about-us">About us</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Navigation;
