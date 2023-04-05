2594768f0e6e75514df9e28732ce84e53516d202
Sina Moradian <sinacsharp@gmail.com>
2023-04-06 02:17:42 +0330

    Create layout header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Navigation from "./Navigation";
+const Header = () => {
+  return (
+    <header>
+      <Navigation />
+      <h2>this is header</h2>
+    </header>
+  );
+};
+
+export default Header;
