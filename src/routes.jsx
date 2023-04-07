a21fef3f5de9692b2a51ac17460b225bbbbeb14e
Sina Moradian <sinacsharp@gmail.com>
2023-04-07 06:08:48 +0330

    Add NotFound page to the routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,13 @@
 import AboutUs from "./pages/AboutUs";
 import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 const routes = [
   { path: "/", component: HomePage, exact: true },
   { path: "/About-us", component: AboutUs },
   { path: "/Profile", component: Profile },
+  { component: NotFound },
+  //we could also use path="*" for Not-Found
 ];
 
 export default routes;
