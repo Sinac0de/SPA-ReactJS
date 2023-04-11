b2d675b99d716d85b22f681bab6ec08c1ffc43d7
Sina Moradian <sinacsharp@gmail.com>
2023-04-11 04:08:59 +0330

    Add blogs page & blog's detail page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,17 @@
+import Blog from "./components/Blog";
 import AboutUs from "./pages/AboutUs";
+import BlogPage from "./pages/BlogPage";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
+
 const routes = [
-  { path: "/", component: HomePage, exact: true },
+  { path: "/blogs/:id", component: Blog },
+  { path: "/blogs", component: BlogPage },
   { path: "/About-us", component: AboutUs },
   { path: "/Profile", component: Profile },
   { component: NotFound },
+  { path: "/", component: HomePage, exact: true },
   //we could also use path="*" for Not-Found
 ];
 
