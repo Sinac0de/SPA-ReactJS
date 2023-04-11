17c7fd9b3367d8e8a5bf471dbb5bd5d7bee531c3
Sina Moradian <sinacsharp@gmail.com>
2023-04-11 04:03:21 +0330

    Add BlogPage

---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+const items = [
+  { name: "blog page 1", to: "/blogs/1" },
+  { name: "blog page 2", to: "/blogs/2" },
+  { name: "blog page 3", to: "/blogs/3" },
+];
+
+const BlogPage = () => {
+  return (
+    <div>
+      <ul>
+        {items.map((item) => {
+          return (
+            <li key={item.to}>
+              <Link to={item.to}>{item.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default BlogPage;
