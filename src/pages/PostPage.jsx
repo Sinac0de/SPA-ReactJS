05a47d847dce3091b536f9b20d6b54f785ea98f5
Sina Moradian <sinacsharp@gmail.com>
2023-04-19 21:07:04 +0330

    Fix params error

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,8 @@
-const PostPage = (props) => {
-  const id = props.match.params.id || 1;
+import { useParams } from "react-router-dom";
+
+const PostPage = () => {
+  const params = useParams();
+  const id = params.id || 1;
   return (
     <div>
       <h2>Post page - {id}</h2>
