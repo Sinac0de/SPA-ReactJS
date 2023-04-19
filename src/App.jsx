e780739aa0a2f0d4695d7898bacbe8157e0a3f0d
Sina Moradian <sinacsharp@gmail.com>
2023-04-19 21:07:32 +0330

    Move all routes to the app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import { useId } from "react";
 import Layout from "./layout/Layout";
 import routes from "./routes";
+import Blog from "./components/Blog";
+import AboutUs from "./pages/AboutUs";
+import BlogPage from "./pages/BlogPage";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import PostPage from "./pages/PostPage";
+import Profile from "./pages/Profile";
+import Downloads from "./components/Downloads";
+import Dashboard from "./components/Dashboard";
+
 import "./App.css";
 
 function App() {
@@ -9,9 +18,16 @@ function App() {
     <Layout>
       {/* check for urls with switch */}
       <Routes>
-        {routes.map((route) => (
-          <Route {...route} key={useId()} />
-        ))}
+        <Route path="/blogs/:id" element={<Blog />} />
+        <Route path="/blogs" element={<BlogPage />} />
+        <Route path="/About-us" element={<AboutUs />} />
+        <Route path="/Profile/*" element={<Profile />}>
+          <Route path="downloads" element={<Downloads />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/post/:id([0-9]+)?" element={<PostPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route element={<NotFound />} />
       </Routes>
     </Layout>
   );
