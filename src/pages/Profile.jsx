274cc88d6d069f4a0f24dfe5635916c14b14bdaf
Sina Moradian <sinacsharp@gmail.com>
2023-04-19 21:07:47 +0330

    Add outlet

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar";
-import Downloads from "../components/Downloads";
-import Dashboard from "../components/Dashboard";
 
 const Profile = () => {
   return (
     <div>
       <SideBar />
-      <Routes>
-        <Route path="downloads" element={<Downloads />}></Route>
-        <Route path="dashboard" element={<Dashboard />}></Route>
-      </Routes>
+      <Outlet />
     </div>
   );
 };
